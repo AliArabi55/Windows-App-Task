1355a87be70b01cf6f6288ebee7ebd9a6e47b6f8
Ali Arabi <aliarabimak@gmail.com>
2024-07-01 23:50:30 +0300

    feat: add empty event handler for label1_Click

--- Windows App Task/program1/program1/Form1.cs
+++ Windows App Task/program1/program1/Form1.cs
@@ -76,5 +76,10 @@ namespace program1
         {
             // Empty event handler
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Empty event handler
+        }
     }
 }
