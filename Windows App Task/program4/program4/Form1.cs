f1199f226608b556560523ee15a0f01323a9beb7
Ali Arabi <aliarabimak@gmail.com>
2024-07-02 23:55:15 +0300

    feat: add btngo_Click event handler in program4

--- Windows App Task/program4/program4/Form1.cs
+++ Windows App Task/program4/program4/Form1.cs
@@ -24,5 +24,29 @@ namespace program4
         {
             // Empty event handler
         }
+
+        private void btngo_Click(object sender, EventArgs e)
+        {
+            lstnumber.Items.Clear();
+            lstnumber.Items.Add("--------------------------------------------------------------");
+            lstnumber.Items.Add(" i\t\t i*2\t\t i*i");
+            lstnumber.Items.Add("--------------------------------------------------------------");
+
+            int number = int.Parse(txtnumber.Text);
+
+            int sum1 = 0;
+            int sum2 = 0;
+            int sum3 = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                lstnumber.Items.Add(i + "\t\t " + i * 2 + "\t\t" + i * i);
+                sum1 += i;
+                sum2 += (i * 2);
+                sum3 += (i * i);
+            }
+            lstnumber.Items.Add("--------------------------------------------------------------");
+            lstnumber.Items.Add(sum1 + "\t\t " + sum2 + "\t\t" + sum3);
+        }
     }
 }
