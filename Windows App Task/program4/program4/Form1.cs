15a76b2c2433327b2d32e9eee4c1e8e54bda531d
Ali Arabi <aliarabimak@gmail.com>
2024-07-02 23:53:28 +0300

    feat: add initial libraries and Form1 constructor in program4

--- Windows App Task/program4/program4/Form1.cs
+++ Windows App Task/program4/program4/Form1.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace program4
+{
+    public partial class Form1 : Form
+    {
+        int y;
+        public Form1()
+        {
+            InitializeComponent();
+            lstnumber.Items.Add("--------------------------------------------------------------");
+            lstnumber.Items.Add(" i\t\t i*2\t\t i*i");
+            lstnumber.Items.Add("--------------------------------------------------------------");
+        }
+    }
+}
