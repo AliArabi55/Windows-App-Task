89c3c1bb638962da341356e43653b82134ec76ae
Ali Arabi <aliarabimak@gmail.com>
2024-07-02 23:38:36 +0300

    feat: add btncheck_Click event handler in program3

--- Windows App Task/program3/program3/Form1.cs
+++ Windows App Task/program3/program3/Form1.cs
@@ -15,5 +15,27 @@ namespace program3
         {
             InitializeComponent();
         }
+
+        private void btncheck_Click(object sender, EventArgs e)
+        {
+            int x;
+            double y;
+            bool num1 = int.TryParse(txtnumber.Text, out x);
+            bool num2 = double.TryParse(txtnumber.Text, out y);
+            if (num1 == true)
+            {
+                lstint.Items.Add(x);
+            }
+            else if (num2 == true)
+            {
+                lstfloat.Items.Add(y);
+            }
+            else
+            {
+                lststring.Items.Add(txtnumber.Text);
+            }
+            txtnumber.Clear();
+            txtnumber.Focus();
+        }
     }
 }
