b1badea09dc5b067d3bdf8c904008495cb73438a
Ali Arabi <aliarabimak@gmail.com>
2024-07-04 23:37:36 +0300

    feat: initialize Form1 class and constructor in program 6

--- Windows App Task/program 6/program 6/Form1.cs
+++ Windows App Task/program 6/program 6/Form1.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace program_6
+{
+    public partial class Form1 : Form
+    {
+        int addarray = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
