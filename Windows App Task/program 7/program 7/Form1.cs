b03c0d0b6976bc65ee4b25838ea525adbeefee9f
Ali Arabi <aliarabimak@gmail.com>
2024-07-08 00:00:43 +0300

    feat: add label2_Click

--- Windows App Task/program 7/program 7/Form1.cs
+++ Windows App Task/program 7/program 7/Form1.cs
@@ -61,5 +61,9 @@ namespace program_7
         private void label1_Click(object sender, EventArgs e)
         {
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
