bcc3bdec4c4d01a3c5402a7bca36e21e08450c71
Ali Arabi <aliarabimak@gmail.com>
2024-07-10 23:54:28 +0300

    feat: add empty event handlers for other controls in program 9

--- Windows App Task/program 9/program 9/Form1.cs
+++ Windows App Task/program 9/program 9/Form1.cs
@@ -52,5 +52,29 @@ namespace program_9
                 txtLocation.Text = "Not";
             }
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void lstArray_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void txtX_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void txtLocation_TextChanged(object sender, EventArgs e)
+        {
+        }
     }
 }
