dc3a5e9deda00c0f251baca4996cdd5d7a213703
Ali Arabi <aliarabimak@gmail.com>
2024-07-10 23:49:22 +0300

    feat: define Form1 constructor in program 9

--- Windows App Task/program 9/program 9/Form1.cs
+++ Windows App Task/program 9/program 9/Form1.cs
@@ -12,5 +12,10 @@ namespace program_9
     public partial class Form1 : Form
     {
         int[] arr = new int[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
     }
 }
